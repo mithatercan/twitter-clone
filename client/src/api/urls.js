2f8ac875fb35f69564836bf7d77d3142248b1f5d
Mithat Ercan <mithat.ercan@hotmail.com>
2021-10-30 00:32:51 +0400

    Add api urls to client

--- client/src/api/urls.js
+++ client/src/api/urls.js
@@ -11,14 +11,16 @@ export const REMOVE_NAME =
 
 export const NEW_POST = "http://localhost:5000/post/new";
 export const EDIT_POST = "http://localhost:5000/post/edit?id=";
+export const DELETE_POST = "http://localhost:5000/post/delete?id=";
 export const LIKE_POST = "http://localhost:5000/post/like?id=";
 export const UNLIKE_POST = "http://localhost:5000/post/unlike?id=";
 export const COMMENT = "http://localhost:5000/post/comment/new";
 export const COMMENT_DELETE = "http://localhost:5000/post/comment/delete?id=";
 
-export const ALL_POSTS = "http://localhost:5000/post";
-export const POST_ID = "http://localhost:5000/post?id=";
-export const POSTS_FOLLOWING = "http://localhost:5000/posts/following";
-export const ALL_PROFILE = "http://localhost:5000/profile";
-export const PROFILE_USERNAME = "http:///localhost:5000/profile?username=";
-export const PROFILE_ID = "http:///localhost:5000/profile?id=";
+export const GET_ALL_POSTS = "http://localhost:5000/post";
+export const GET_POST_ID = "http://localhost:5000/post?id=";
+export const GET_FOLLOWING_POSTS = "http://localhost:5000/posts/following";
+
+export const GET_ALL_PROFILES = "http://localhost:5000/profile";
+export const GET_PROFILE_USERNAME = "http:///localhost:5000/profile?username=";
+export const GET_PROFILE_ID = "http:///localhost:5000/profile?id=";
